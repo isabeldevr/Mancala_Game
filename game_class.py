4a53b1c4fef393c9ce068ed4e1bbaf74f8212547
isabeldevalenzuela <idevalenzuel.ieu2022@student.ie.edu>
2023-11-14 02:09:33 +0100

    Isabel's work2 14/11/2023

--- game_class.py
+++ game_class.py
@@ -5,6 +5,7 @@ COLUMNS = 8
 CELL_SIZE = 68
 CELL_SPACING = 66
 
+
 class Game:
 
     def __init__(self):
@@ -20,7 +21,6 @@ class Game:
         self.board.on_start = self.start
         self.board.on_key_press = self.keyboard_command
         self.board.on_mouse_click = self.mouse_click
-
 
     def start(self) -> None:
         """Initializes the game."""
@@ -38,7 +38,6 @@ class Game:
         self.current_player = 1
         self.board.print("Let's play Mancala!")
 
-
     def keyboard_command(self, key) -> None:
         """Handle start of game"""
         if key == "q":
@@ -48,11 +47,9 @@ class Game:
             self.start()
         ## REVISE THIS METHOD
 
-
     def draw_board(self) -> None:
         """Draws the board"""
         self.board.show()
-
 
     def mouse_click(self, btn: int, row: int, col: int) -> (int, int):
         """Handles mouse clicks"""
@@ -66,13 +63,16 @@ class Game:
 
         if self.current_player == 1 and row != 2:
             self.board.print("Invalid move! Try again.")
-        else:
+            return None
+
+        if self.current_player == 1:
+            self.board.print("")
             return self.moving_stones(row, col)
 
         if self.current_player == 2:
+            self.board.print("")
             row, col = self.min_max_ai_player()
             return self.moving_stones(row, col)
-
 
     def moving_stones(self, row: int, col: int) -> (int, int):
         """Moves stones around the board"""
@@ -93,24 +93,23 @@ class Game:
             if row == 1 and col == 7:
                 row = 2
                 col = 1
-            self.board[row][col] += 1
+            self.board[row][col] = int(self.board[row][col]) + 1
             stones -= 1
         last_row, last_col = row, col
-        if not self.check_game_over():
+        continue_game = self.check_game_over()
+        if continue_game:
             self.stone_capture(last_row, last_col)
             self.current_player_update(last_row, last_col)
         ## WE SHOULD ALSO FIND A BETTER TRANSVERSAL METHOD
-
 
     def min_max_ai_player(self) -> (int, int):
         current_board_state = [[0 for _ in range(COLUMNS)] for _ in range(ROWS)]
         for row in range(1, 7):
             for col in range(1, 7):
                 current_board_state[row][col] = self.board[row][col]
-        ## return best row, column
-        ## here we can do the min max player
+        # return best row, column
+        # here we can do the min max player
         pass
-
 
     def stone_capture(self, last_row: int, last_col: int) -> None:
         """Checks if we can capture stones"""
@@ -124,8 +123,7 @@ class Game:
             self.board[2][last_col] = 0
         else:
             pass
-        ## REFINE THIS METHOD
-
+        # REFINE THIS METHOD
 
     def current_player_update(self, last_row: int, last_col: int) -> None:
         """Updates the current player based on the last move"""
@@ -139,11 +137,10 @@ class Game:
             # Switch to the other player's turn
             self.current_player = 3 - self.current_player
             self.board.print("Player " + str(self.current_player) + "'s turn!")
-        ## REVISE THIS METHOD
-
+        # REVISE THIS METHOD
 
     def check_game_over(self) -> bool:
-        "Checks if the game is over and if so declares a winner"
+        """Checks if the game is over and if so declares a winner"""
         if all(self.board[1][col] == 0 for col in range(1, 7)):
             self.declare_winner(2)
             return True
@@ -152,19 +149,19 @@ class Game:
             return True
         return False
 
-
-    def declare_winner(self, player: int):
-        "This method declares the winner"
+    def declare_winner(self, player: int) -> None:
+        """This method declares the winner"""
         if player == 1:
             self.board.print("Player 1 wins!")
         else:
             self.board.print("CPU wins!")
-        ## ADD LINK TO RE-START OR QUIT FUNCTION
+        self.board.close()
+        # ADD LINK TO RE-START OR QUIT FUNCTION
 
 ## SOME NOTES
 # 1. We need to implement the min max player
 # 2. We need to fix the quit + restart button (so u can quit/restart at any time) + at the end of th game
 # 3. We need to refine the stone capture method (look at how they did in YT Video)
 # 4. We need to refine the transveral method (look at how they did in YT Video)
-# 5. We need to adjust the player update metehod (so that u cnanpt clik outside your turn)
+# 5. We need to adjust the player update method (so that u cant clik outside your turn)
 # 6. We need to add the stone images to the board (so layer the stones instead of the numbers)
