3b70820d8447d7cba00080a43f23ea1c03e0d878
Torkuno <tmesalles@hotmail.com>
2023-11-22 15:48:05 +0100

    Commit changes 11/22/2023

--- game_class.py
+++ game_class.py
@@ -2,41 +2,67 @@ from game2dboard import Board
 
 ROWS = 4
 COLUMNS = 8
-CELL_SIZE = 68
-CELL_SPACING = 66
+CELL_SIZE = 117
+CELL_SPACING = 10
+
+# INVALID_ROWS = [0,3]  # Might not be needed, we can just do "not in CUP_ROWS"
+P1_ROW = 2
+P1_COL = 7
+P2_ROW = 1
+P2_COL = 0
+CUP_ROWS = [P2_ROW, P1_ROW]
+HOME_COLUMNS = [P2_COL,P1_COL]
+CUP_COLUMNS = range(1,7)  #[1,2,3,4,5,6]
 
 
 class Game:
-
     def __init__(self):
         self.board = Board(ROWS, COLUMNS)
         self.board.title = "Mancala"
+
         self.board.cell_size = CELL_SIZE
         self.board.cell_spacing = CELL_SPACING
         self.board.margin = 0
+
         self.board.background_image = "mancala_board.png"
         if not self.board.background_image:
             self.board.cell_color = "bisque"
+
         self.board.create_output(background_color="black", color="bisque", font_size=12)
+
         self.board.on_start = self.start
         self.board.on_key_press = self.keyboard_command
         self.board.on_mouse_click = self.mouse_click
 
+        #self.board_slots = {i: 4 for i in range(1, 13)}
+
+
+
     def start(self) -> None:
         """Initializes the game."""
-        # Initialize pits with 4 stones each
-        for row in range(1, 3):
+
+        self.p2_type = 0  # Default Player 2 type is HUMAN
+
+        for row in range(1, 3):  # Initialize pits with 4 stones each
             for col in range(1, 7):
                 self.board[row][col] = 4
 
         # Set player's cups to 0
-        self.board[1][0] = "\t Player 2 CPU\n Home Pit"
-        self.board[2][0] = 0
-        self.board[1][7] = 0
-        self.board[2][7] = "\t Player 1\n Home Pit"
+        self.board[1][0] = 0
+        self.board[2][0] = "Player 2\nHome Pit"
+        self.board[1][7] = "Player 1\nHome Pit"
+        self.board[2][7] = 0
 
         self.current_player = 1
-        self.board.print("Let's play Mancala!")
+        self.board.print(f"Let's play Mancala! Player {self.current_player}, goes first!")
+
+        self.board_dictionary = {
+            "Row_1": [4, 4, 4, 4, 4, 4],
+            "Row_2": [4, 4, 4, 4, 4, 4],
+            "Player1_score": 0,
+            "Player2_score": 0,
+        }
+
 
     def keyboard_command(self, key) -> None:
         """Handle start of game"""
@@ -45,81 +71,94 @@ class Game:
         elif key == "r":
             self.board.clear()
             self.start()
-        ## REVISE THIS METHOD
+
 
     def draw_board(self) -> None:
         """Draws the board"""
         self.board.show()
 
-    def mouse_click(self, btn: int, row: int, col: int) -> (int, int):
+
+    def mouse_click(self, btn, row: int, col: int):
         """Handles mouse clicks"""
-        if self.board[row][col] == 0:
-            self.board.print("Invalid move! Try again.")
+        if row not in CUP_ROWS:
             return None
 
-        if col == 0 or col == 7 or row == 0 or row == 3:
+        if (col in HOME_COLUMNS) and (row in CUP_ROWS):
             self.board.print("You cannot click on a home!")
             return None
 
-        if self.current_player == 1 and row != 2:
-            self.board.print("Invalid move! Try again.")
-            return None
-
         if self.current_player == 1:
+            if row != P1_ROW:
+                self.board.print("This cup belongs to Player 2!\nTry again.")
+                return None
+            if self.board[row][col] == 0:
+                self.board.print("This cup is empty!\nTry again.")
+                return None
             self.board.print("")
             return self.moving_stones(row, col)
 
         if self.current_player == 2:
+            # if self.p2_type == 1:
+                # self.p2_ai(row, col)
+            if row != P2_ROW:
+                self.board.print("This cup belongs to Player 1!\nTry again.")
+                return None
+            if self.board[row][col] == 0:
+                self.board.print("This cup is empty!\nTry again.")
+                return None
             self.board.print("")
-            row, col = self.min_max_ai_player()
             return self.moving_stones(row, col)
+
 
     def moving_stones(self, row: int, col: int) -> (int, int):
         """Moves stones around the board"""
-        stones = self.board[row][col]
-        # remove stones from pit
-        self.board[row][col] = 0
-        while stones > 0:
-            col += 1
-            # for counter-clock movement
-            if row == 1:
-                col -= 2
-            # base cases for when we reach the end of the board
-            if row == 2 and col == 7:
-                row = 1
-                col = 6
-                self.board[1][7] += 1
-                stones -= 1
-            if row == 1 and col == 7:
-                row = 2
-                col = 1
+        tempStones = self.board[row][col]
+        self.board[row][col] = 0  # remove stones from pit
+
+        while tempStones > 0:
+            if row == P1_ROW:  # clockwise movement
+                col += 1
+            else:  # counter-clock movement
+                col -= 1
+
+            if (row == P1_ROW) and (col == P1_COL):  # reach the end of the RIGHT side
+                self.board[P1_ROW][P1_COL] += 1
+                row = P2_ROW
+                col = P1_COL - 1
+                tempStones -= 1
+            if (row == P2_ROW) and (col == P2_COL):  # reach the end of the LEFT side
+                self.board[P2_ROW][P2_COL] += 1
+                row = P1_ROW
+                col = P2_COL + 1
+                tempStones -= 1
             self.board[row][col] = int(self.board[row][col]) + 1
-            stones -= 1
+            tempStones -= 1
 
         last_row, last_col = row, col
-        continue_game = self.check_game_over()
-        if not continue_game:
-            self.stone_capture(last_row, last_col)
+        if not self.check_game_over():
+            #self.stone_capture(last_row, last_col)
             self.current_player_update(last_row, last_col)
 
         ## WE SHOULD ALSO FIND A BETTER TRANSVERSAL METHOD
 
-    def min_max_ai_player(self) -> (int, int):
+
+    def p2_ai(self) -> (int, int):
         current_board_state = [[0 for _ in range(COLUMNS)] for _ in range(ROWS)]
         for row in range(1, 7):
             for col in range(1, 7):
                 current_board_state[row][col] = self.board[row][col]
         # return best row, column
         # here we can do the min max player
-        pass
+        return None
+
 
     def stone_capture(self, last_row: int, last_col: int) -> None:
         """Checks if we can capture stones"""
-        if last_row == 1 and self.board[last_row][last_col] == 1 and self.board[2][last_col] != 0:
+        if (last_row == 1) and (self.board[last_row][last_col] == 1) and (self.board[2][last_col] != 0):
             self.board[1][7] += self.board[2][last_col] + 1
             self.board[2][last_col] = 0
             self.board[1][last_col] = 0
-        elif last_row == 2 and self.board[last_row][last_col] == 1 and self.board[1][last_col] != 0:
+        elif (last_row == 2) and (self.board[last_row][last_col] == 1) and (self.board[1][last_col] != 0):
             self.board[2][7] += self.board[1][last_col] + 1
             self.board[1][last_col] = 0
             self.board[2][last_col] = 0
@@ -127,44 +166,54 @@ class Game:
             pass
         # REFINE THIS METHOD
 
+
     def current_player_update(self, last_row: int, last_col: int) -> None:
         """Updates the current player based on the last move"""
-        if last_row == 1 and last_col == 7:
+        if last_row == 1 and last_col == 7: # P1 lands on home cup
             self.current_player = 1
             self.board.print("Player 1 goes again!")
-        elif last_row == 2 and last_col == 7:
+        elif last_row == 2 and last_col == 7: # P2 lands on home cup
             self.current_player = 2
-            self.board.print("CPU, Player 2, goes again!")
+            self.board.print("Player 2 goes again!")
         else:
             # Switch to the other player's turn
             self.current_player = 3 - self.current_player
-            self.board.print("Player " + str(self.current_player) + "'s turn!")
+            self.board.print(f"Player {str(self.current_player)}'s turn!")
         # REVISE THIS METHOD
+
 
     def check_game_over(self) -> bool:
         """Checks if the game is over and if so declares a winner"""
-        if all(self.board[1][col] == 0 for col in range(1, 7)):
+        if all(self.board[1][col] == 0 for col in CUP_COLUMNS):
             self.declare_winner(2)
             return True
-        elif all(self.board[2][col] == 0 for col in range(1, 7)):
+        elif all(self.board[2][col] == 0 for col in CUP_COLUMNS):
             self.declare_winner(1)
             return True
         return False
+
 
     def declare_winner(self, player: int) -> None:
         """This method declares the winner"""
         if player == 1:
             self.board.print("Player 1 wins!")
         else:
-            self.board.print("CPU wins!")
+            self.board.print("Player 2 wins!")
         # for now we close the board when finished
         self.board.close()
         # ADD LINK TO RE-START OR QUIT FUNCTION
 
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
+    game.draw_board()
+
+
 ## SOME NOTES
-# 1. We need to implement the min max player
-# 2. We need to fix the quit + restart button (so u can quit/restart at any time) + at the end of th game
-# 3. We need to refine the stone capture method (look at how they did in YT Video)
-# 4. We need to refine the transveral method (look at how they did in YT Video)
-# 5. We need to adjust the player update method (so that u cant clik outside your turn) + sometimes message not showing
-# 6. We need to add the stone images to the board (so layer the stones instead of the numbers)
+# 1. Implement the min max player
+# 2. Fix the quit + restart button (so u can quit/restart at any time) + at the end of th game
+# 3. Refine the stone capture method (look at how they did in YT Video)
+# 4. Refine the transveral method (look at how they did in YT Video)
+# 5. Adjust the player update method (so that u cant clik outside your turn) + sometimes message not showing
+# 6. Add the stone images to the board
