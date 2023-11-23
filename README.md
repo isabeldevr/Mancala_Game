1762b6424fcab2698b2756fcb8b4c5f3837a2362
isabeldevalenzuela <idevalenzuel.ieu2022@student.ie.edu>
2023-11-23 01:25:09 +0100

    Isabel's work 22/11/2023

--- README.md
+++ README.md
@@ -1,30 +1,79 @@
-## RULES OF THE GAME
 
-**Basic Rules**
-* Play always moves around the board in a counter-clockwise circle (to the right)
-* The store on your right belongs to you. That is where you keep the seeds you win.
-* The six pits near you are your pits.
-* Only use one hand to pick up and put down seeds.
-* Once you touch the seeds in a pit, you must move those seeds.
-* Only put seeds in your own store, not your opponent’s store
+# Mancala Game
 
-**Starting the Game**
+## Rules of the Game
 
-On a turn, a player picks up all the seeds in one pit and “sows” them to the right, placing one
-seed in each of the pits along the way. If you come to your store, then add a seed to your store
-and continue. You may end up putting seeds in your opponent’s pits along the way.
-Play alternates back and forth, with opponents picking up the seeds in one of their pits and
-distributing them one at a time into the pits on the right, beginning in the pit immediately to the
-right.
+### Game Setup:
 
-**Special Rules**
-* When the last seed in your hand lands in your store, take another turn.
-* When the last seed in your hand lands in one of your own pits, if that pit had been empty you
-get to keep all of the seeds in your opponents pit on the opposite side. Put those captured seeds,
-as well as the last seed that you just played on your side, into the store.
+- Two rows with six pits each.
+- Player 1 (top row, CPU Player) and Player 2 (bottom row, Human Player).
+- Each player has a home (leftmost and rightmost pits) which keeps track of their points.
+- The game starts out with 4 stones per pit.
 
-**Ending the Game**
+### Objective:
+- Capture more stones than your opponent.
+- Game ends when all pits on one side are empty.
 
-The game is over when one player’s pits are completely empty. The other player takes the seeds
-remaining in her pits and puts those seeds in her store. Count up the seeds. Whoever has the most
-seeds wins
+### Gameplay:
+
+#### Starting the Game:
+
+- Players take turns; 
+- Player 2 (so the human player) starts.
+
+#### Turn Mechanics:
+
+- Select a pit, pick up its stones, and distribute counterclockwise. This is all simulated in one click.
+- If the last stone lands in the player's home, they get another turn.
+
+#### Capturing Stones:
+
+- If the last stone lands in an empty pit on the player's side, and the opposite pit on the opponent's side has stones, capture all those stones.
+
+#### End Game:
+
+- Game ends when one player's pits are empty.
+- Remaining stones on the opponent's side are captured.
+
+#### Determining the Winner:
+
+- Count seeds in the store; player with the most seeds wins.
+
+## Implementation:
+
+### Initializing the Game:
+
+- Initialize pits with stones using nested loops (2x6). Complexity: O(1).
+
+### Handling Player Inputs:
+
+- Methods handle user inputs and perform validation.
+
+### Moving Stones:
+
+- Stone movement based on the number of stones and their path. Worst-case complexity: O(stones).
+
+### Stone Capture:
+
+- Checks conditions to capture stones. Complexity depends on conditions and stone count. Worst case: O(1) or O(n).
+
+### Game Over Check:
+
+- Checks if all pits for a player are empty. Complexity: O(1) or O(n).
+
+### Declaring Winner:
+
+- Print winner and close the board. Complexity: O(1).
+
+## Key Data Structures:
+
+### Matrices:
+
+- Represent the game board; used to store the visual/image representation of the number of stones in each pit.
+- Accessed and modified to visually simulate stone movement.
+
+### Dictionary (board_update method):
+
+- Serves to easily simulate the counterclockwise movement of mancala.
+- Helps the visual board representation by translating board_dictionary to respective pits and scores in the matrix.
+- Manages the logical state of the board independently of the visual interface.
--- game_class.py
+++ game_class.py
@@ -1,9 +1,12 @@
 from game2dboard import Board
+import random
+import copy
 
 ROWS = 4
 COLUMNS = 8
-CELL_SIZE = 68
-CELL_SPACING = 66
+CELL_SIZE = 117
+CELL_SPACING = 10
+STONE_IMAGE_FILES = [f"{i}.png" for i in range(0, 48)] # TOMAS: 48 is the maximum stones u could ever have so we need up to 48 this sucks so sorry)
 
 
 class Game:
@@ -22,26 +25,31 @@ class Game:
         self.board.on_start = self.start
         self.board.on_key_press = self.keyboard_command
         self.board.on_mouse_click = self.mouse_click
+        self.board_dictionary = {}
+        self.board.cursor = "arrow"
 
     def start(self) -> None:
-        """Initializes the game."""
+        """Initializes the game.
+        input: None
+        output: None. Calls the draw_board method with the initial board"""
         # Initialize pits with 4 stones each
+
+        ### TOMÁS CHANGE THIS BY ALL IMAGES OF 4 FOR WHEN WE START THE GAME
         for row in range(1, 3):
             for col in range(1, 7):
                 self.board[row][col] = 4
 
         # Set player's cups to 0
-        self.board[1][0] = "\t Player 1 CPU\n Home Pit"
+        self.board[1][0] = "Player 1"
         self.board[2][0] = 0
         self.board[1][7] = 0
-        self.board[2][7] = "\t Player 2\n Home Pit"
+        self.board[2][7] = "Player 2, You"
 
-        # Initialising the player
+        # Initialising the player: human player goes first
         self.current_player = 2
         self.board.print(f"Let's play Mancala! \t Player {self.current_player}, goes first!")
 
-        ## Initialise the board dictionary
-        ## This is what we
+        # Initialise the board dictionary
         self.board_dictionary = {
             "Row_1": [4, 4, 4, 4, 4, 4],
             "Row_2": [4, 4, 4, 4, 4, 4],
@@ -50,23 +58,24 @@ class Game:
         }
 
     def keyboard_command(self, key) -> None:
-        """Handle start of game"""
+        """Handle quitting and re-start of game
+        input: key pressed
+        output: None. Calls the start or quit method"""
         if key == "q":
             self.board.close()
         elif key == "r":
             self.board.clear()
             self.start()
-        ## REVISE THIS METHOD
 
     def draw_board(self) -> None:
         """Draws the board"""
         self.board.show()
 
-    def mouse_click(self, btn: int, row: int, col: int) -> (int, int):
-        """Handles mouse clicks"""
+    def mouse_click(self, btn: int, row: int, col: int) -> None:
+        """Handles mouse clicks
+        input: btn, row clicked, col clicked
+        output: None. Calls the moving_stones method"""
         if self.current_player == 1:
-            self.board.print("It is not your turn! Player 1 (CPU) is playing")
-            self.moving_stones(*self.ai_player())
             return None
 
         if self.board[row][col] == 0 or row != 2 or col in {0, 7} or row in {0, 3}:
@@ -76,101 +85,170 @@ class Game:
         self.board.print("")
         return self.moving_stones(row, col)
 
-    def moving_stones(self, row: int, col: int) -> (int, int):
-        """Moves stones around the board"""
+    def moving_stones(self, row: int, col: int) -> None:
+        """Moves stones around the board:
+        input: row of chosen cell, column  of chosen cell
+        output: None. Calls the board_update method """
+        start_row = row
         col -= 1
+        start_col = col
         stones = self.board_dictionary[f"Row_{self.current_player}"][col]
+
         # remove stones from pit
-        self.board_dictionary[f"Row_{self.current_player}"][col] = 0
-        row = self.current_player
+        self.board_dictionary[f"Row_{row}"][col] = 0
 
         while stones > 0:
             # Move to the next column
             col += 1
-
             # Check if we reached the end of the row
             if col >= len(self.board_dictionary[f"Row_{row}"]):
                 self.board_dictionary[f"Player{self.current_player}_score"] += 1
                 row = 3 - row
                 col = 0
-
             # Update the count of stones in the current cell
             self.board_dictionary[f"Row_{row}"][col] += 1
             stones -= 1
 
-        self.board_update(self.board_dictionary)
+        # Check if we can go again
         last_row, last_col = row, col
         continue_game = self.check_game_over()
         if not continue_game:
-            self.stone_capture(last_row, last_col)
+            self.stone_capture(start_row, start_col, last_row, last_col, self.board_dictionary)
             self.current_player_update(last_row, last_col)
 
+        # Update the board
+        return self.board_update(self.board_dictionary)
+
     def board_update(self, board_dictionary: dict) -> None:
-        """Updates the board"""
-       ## make it so that this updates the board correctly
+        """Updates the board
+        input: board_dictionary
+        output: None"""
         for col in range(1, 7):
-            self.board[1][COLUMNS-col-1] = board_dictionary["Row_1"][col - 1]
+            self.board[1][COLUMNS - col - 1] = board_dictionary["Row_1"][col - 1]
             self.board[2][col] = board_dictionary["Row_2"][col - 1]
+            ## TOMÁS USE THIS LOOP TO UPDATE THE IMAGES
         self.board[1][7] = board_dictionary["Player1_score"]
         self.board[2][0] = board_dictionary["Player2_score"]
-        # put the update images function here or integrate
-        # use an array [0, 1, 2, 3, 4, 5, 6] to access the correct images
+
 
     def ai_player(self) -> (int, int):
-        """This method is the AI player"""
-        # we can use heap and priority queues
-        pass
+        """This method is the AI player
+        input: None
+        output: best move coordinates (row, col)"""
+        return self.make_best_move(self.current_player, 1)
 
-    def stone_capture(self, last_row: int, last_col: int) -> None:
-        """Checks if we can capture stones"""
-        if last_row == 1 and self.board[last_row][last_col] == 1 and self.board[2][last_col] != 0:
-            self.board[1][7] += self.board[2][last_col] + 1
-            self.board[2][last_col] = 0
-            self.board[1][last_col] = 0
-        elif last_row == 2 and self.board[last_row][last_col] == 1 and self.board[1][last_col] != 0:
-            self.board[2][7] += self.board[1][last_col] + 1
-            self.board[1][last_col] = 0
-            self.board[2][last_col] = 0
-        else:
-            pass
-        # REFINE THIS METHOD
+    def make_best_move(self, player_to_evaluate, depth) -> (int, int):
+        """This method makes the best move
+        input: player_to_evaluate, depth
+        output: best move coordinates (row, col)"""
+
+        # base case to stop recursion
+        if depth <= 0:
+            return random.choice(self.possible_moves_by_player(player_to_evaluate))
+
+        possible_moves = self.possible_moves_by_player(player_to_evaluate)
+        if possible_moves:
+            # Evaluate each move and choose the one with the highest score
+            best_move = max(possible_moves, key=lambda move: self.evaluate_move(move, depth=depth-1))
+            print(best_move)
+            return best_move[0], best_move[1]
+
+    def possible_moves_by_player(self, player_to_evaluate) -> list:
+        """This method returns the possible moves by player
+        input: player_to_evaluate
+        output: list"""
+        values = []
+        for col in range(6):
+            values.append((player_to_evaluate, col))
+        return values
+
+    def evaluate_move(self, move, depth) -> int:
+        """ This method evaluates the move
+        input: move, depth of recursion
+        output: points obtained for the move"""
+        dictionary_copy = copy.deepcopy(self.board_dictionary)
+        points = 0
+        stones = dictionary_copy[f"Row_{self.current_player}"][move[1]]
+
+        # remove stones from pit
+        dictionary_copy[f"Row_{self.current_player}"][move[1]] = 0
+        row = self.current_player
+        col = move[1]
+
+        while stones > 0:
+            # Move to the next column
+            col += 1
+            # Check if we reached the end of the row
+            if col >= len(dictionary_copy[f"Row_{row}"]):
+                dictionary_copy[f"Player{self.current_player}_score"] += 1
+                points += 1
+                row = 3 - row
+                col = 0
+            # Update the count of stones in the current cell
+            dictionary_copy[f"Row_{row}"][col] += 1
+            stones -= 1
+
+        # check if we capture stones
+        last_row, last_col = row, col
+        self.stone_capture(move[0], move[1], last_row, last_col, dictionary_copy)
+
+        if depth > 0:
+            opponent_moves = self.make_best_move(3 - self.current_player, depth - 1)
+            points -= self.evaluate_move(opponent_moves, depth - 1)
+        return points
+
+    def stone_capture(self, start_row: int, start_col: int, last_row: int, last_col: int, dictionary: dict) -> None:
+        """ Checks if we can capture stones by checking if we land in an empty pit on our side
+        input: starting row, starting column, last row, last column, dictionary
+        output: None """
+        if start_row == last_row and start_col != last_col and dictionary[f"Row_{last_row}"][last_col] == 1:
+            dictionary[f"Row_{last_row}"][last_col], dictionary[f"Row_{3 - last_row}"][last_col] = \
+                dictionary[f"Row_{last_row}"][last_col], dictionary[f"Row_{3 - last_row}"][last_col]
+        return self.board[last_row][last_col]
 
     def current_player_update(self, last_row: int, last_col: int) -> None:
-        """Updates the current player based on the last move"""
-        # string = "Player " + str(self.current_player) + "'s turn!" if self.current_player == 1 else "CPU, Player 2, goes again!"
-        if last_row == 1 and last_col == 7:
+        """Updates the current player based on the last move
+        input: last row, last column
+        output: None"""
+        if last_row == 2 and last_col == 6:
             self.current_player = 1
             self.board.print("Player 1, CPU goes again!")
-        elif last_row == 2 and last_col == 7:
+        elif last_row == 1 and last_col == 0:
             self.current_player = 2
-            self.board.print("Player 2, goes again!")
+            self.board.print("You can go again!")
+            self.board.cursor = "arrow"
         else:
-            # Switch to the other player's turn
             self.current_player = 3 - self.current_player
-            self.board.print("Player " + str(self.current_player) + "'s turn!")
-        # REVISE THIS METHOD
+
+        # Move the following line outside the if condition
+        self.board.print("Player {}'s turn".format(self.current_player))
+
+        if self.current_player == 1:
+            self.cursor = None
+            row, col = self.ai_player()
+            return self.moving_stones(row, col + 1)
 
     def check_game_over(self) -> bool:
-        """Checks if the game is over and if so declares a winner"""
-        if all(self.board[1][col] == 0 for col in range(1, 7)):
-            self.declare_winner(1)
-            return True
-        elif all(self.board[2][col] == 0 for col in range(1, 7)):
-            self.declare_winner(2)
-            return True
+        """Checks if the game is over and if so declares a winner. First to finish is always the winner.
+        input: None
+        output: bool"""
+        for row in range(1, 3):
+            if self.board_dictionary[f"Row_{row}"] == [0, 0, 0, 0, 0, 0]:
+                return self.declare_winner(row)
         return False
 
-    def declare_winner(self, player: int) -> None:
-        """This method declares the winner"""
-        self.board.print(f"Player{player} wins!")
-        # ADD LINK TO RE-START OR QUIT FUNCTION
+    def declare_winner(self, player: int) -> bool:
+        """This method declares the winner
+        input: first player to finish
+        output: bool"""
+        self.board.print(f"Player {player} wins! Congratulations! \n Press 'r' to restart or 'q' to quit")
+        return True
 
-## SOME NOTES
-# 1. We need to implement the min max, trees player
-# 2. We need to fix the quit + restart button (so u can quit/restart at any time) + at the end of th game
-# 3. We need to refine the stone capture method (look at how they did in YT Video)
-# 4. We need to refine the transveral method (look at how they did in YT Video)
-# 5. We need to adjust the player update method (so that u cant clik outside your turn) + sometimes message not showing
+
+# SOME NOTES
+# current player revise method (turns are a bit wonky, its nit wasy to see who is playing i want to introduce a delay )
+# Ask teacher what kind of doscstrings he prefers
 # 6. We need to add the stone images to the board (so layer the stones instead of the numbers)
 #      -> `create a method for ths and put images into a list access according to necessity
-
+# 7.   Optimise the code (make it more efficient) where it says revise
+#  8. make so that u cannot land in opponents points (just make sure)
